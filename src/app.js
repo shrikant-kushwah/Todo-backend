e63db568ce4f430b466741590d84fd74bfa47af5
shrikant-kushwah <shrikant20052001@gmail.com>
2025-09-01 11:59:55 +0530

    deploy setup on vercel

--- src/app.js
+++ src/app.js
@@ -10,20 +10,44 @@ const cookieParser = require("cookie-parser")
 
 
 
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("DB connected")
-    app.listen(process.env.PORT, () => {
-      console.log("Server is Running on " + process.env.PORT)
-    })
-  })
-  .catch(() => {
-    console.log("Failed")
-  })
+// mongoose.connect(process.env.MONGO_URL)
+//   .then(() => {
+//     console.log("DB connected")
+//     app.listen(process.env.PORT, () => {
+//       console.log("Server is Running on " + process.env.PORT)
+//     })
+//   })
+//   .catch(() => {
+//     console.log("Failed")
+//   })
 
-
-app.use(cors({credentials:true, origin:"http://localhost:5173"}))
+// middlewares
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }))
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api", todoRouter)
 app.use("/api", userRouter)
+
+
+// local development mode
+if (require.main === module) {
+  mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+      console.log("DB connected");
+      app.listen(process.env.PORT, () => {
+        console.log("Server running on " + process.env.PORT);
+      });
+    })
+    .catch(() => {
+      console.log("DB connection failed");
+    });
+} else {
+
+  //vercel mode (no app.listen, just DB connect)
+  mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("DB connected (Vercel)"))
+    .catch(() => console.log("DB connection failed (Vercel)"));
+}
+
+
+module.exports = app;
